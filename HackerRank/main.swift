3e37e3fcb63df0f215d077ab0135623bad234eec
VladBuhaescu <vlad.buhaescu@tide.co>
2019-02-11 11:39:47 +0000

    setup for base project

--- HackerRank/main.swift
+++ HackerRank/main.swift
@@ -1,12 +1,43 @@
-//
-//  main.swift
-//  HackerRank
-//
-//  Created by Vlad-Constantin Buhaescu on 11/02/2019.
-//  Copyright © 2019 Vlad-Constantin Buhaescu. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+// Complete the matchingStrings function below.
+func matchingStrings(strings: [String], queries: [String]) -> [Int] {
+    
+    return [1,2,3]
+}
+
+func start() {
+    let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+    FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+    let fileHandle = FileHandle(forWritingAtPath: stdout)!
+    
+    print("give strings number: ")
+    let new = readLine()
+    guard let stringsCount = Int((new?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+        else { fatalError("Bad input") }
+    
+    print("give string: ")
+    let strings: [String] = AnyIterator{ readLine() }.prefix(stringsCount).map { $0 }
+    
+    guard strings.count == stringsCount else { fatalError("Bad input") }
+    
+    print("give querries number: ")
+    guard let queriesCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+        else { fatalError("Bad input") }
+    
+    print("give query: ")
+    let queries: [String] = AnyIterator{ readLine() }.prefix(queriesCount).map {
+        $0
+    }
+    
+    guard queries.count == queriesCount else { fatalError("Bad input") }
+    
+    let res = matchingStrings(strings: strings, queries: queries)
+    
+    fileHandle.write(res.map{ String($0) }.joined(separator: "\n").data(using: .utf8)!)
+    fileHandle.write("\n".data(using: .utf8)!)
+}
+
+start()
+
 
