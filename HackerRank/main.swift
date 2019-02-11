2d14049f7a6310a7d92db8320abc0b8c37fac5cf
VladBuhaescu <vlad.buhaescu@tide.co>
2019-02-11 11:05:44 +0000

    Initial Commit

---
+++ HackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Vlad-Constantin Buhaescu on 11/02/2019.
+//  Copyright © 2019 Vlad-Constantin Buhaescu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
